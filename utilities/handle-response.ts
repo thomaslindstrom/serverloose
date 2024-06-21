4f8661a37da147c4539554c17f40d7df255eaa86
Thomas Lindstrøm <t@hom.as>
2024-06-21 16:24:05 +0200

    Handle implicit JSON

--- utilities/handle-response.ts
+++ utilities/handle-response.ts
@@ -17,7 +17,17 @@ export default function handleResponse({response}: Context, body: any = {}) {
 	response.statusCode = 200;
 
 	if (response.hasHeader('content-type')) {
-		response.end(body);
+		const header = response.getHeader('content-type');
+
+		if (
+			typeof header === 'string' &&
+			typeof body !== 'string' &&
+			['application/json', 'application/json; charset=utf-8'].includes(header)
+		) {
+			response.end(JSON.stringify({success: true, ...body}));
+		} else {
+			response.end(body);
+		}
 	} else {
 		response.setHeader('content-type', 'application/json; charset=utf-8');
 		response.end(JSON.stringify({success: true, ...body}));
